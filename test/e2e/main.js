098659c584cd270f954139541a418b5084f84487
JulianMayorga <mayorga.julian@gmail.com>
2013-09-09 10:12:49 -0300

    Toggle done

--- test/e2e/main.js
+++ test/e2e/main.js
@@ -10,22 +10,34 @@ describe('Lean todo app', function () {
     expect(browser().location().url()).toBe('/');
   });
 
-  it('should add a task to the list', function () {
-    expect(element('.task').count()).toEqual(0);
-    input('newTask').enter('do laundry');
-    element('#new-task-btn').click();
-    expect(element('.task').count()).toEqual(1);
-    expect(element('.task input').val()).toEqual('do laundry');
+  describe('Add task :', function () {
+    it('should add a task to the list', function () {
+      expect(element('.task').count()).toEqual(0);
+      input('newTask').enter('do laundry');
+      element('#new-task-btn').click();
+      expect(element('.task').count()).toEqual(1);
+      expect(element('.task .text').text()).toEqual('do laundry');
+    });
+
+    it('should start with an empty task input', function () {
+      expect(input('newTask').val()).toEqual('');
+    });
+
+    it('should clean the input value after adding a task', function () {
+      input('newTask').enter('do laundry');
+      element('#new-task-btn').click();
+      expect(input('newTask').val()).toEqual('');
+    });
   });
 
-  it('should start with an empty task input', function () {
-    expect(input('newTask').val()).toEqual('');
-  });
-
-  it('should clean the input value after adding a task', function () {
-    input('newTask').enter('do laundry');
-    element('#new-task-btn').click();
-    expect(input('newTask').val()).toEqual('');
+  describe('Toggle done: ', function () {
+    it('should mark a task as done when checked', function () {
+      input('newTask').enter('do laundry');
+      element('#new-task-btn').click();
+      input('task.done').check();
+      expect(element('.text').attr('class')).toContain('done-true');
+      expect(element('.text').css('text-decoration')).toBe('line-through');
+    });
   });
 
 });
