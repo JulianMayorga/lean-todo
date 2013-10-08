579c47509cdd216ca92e4335a092e80127bb2a5d
JulianMayorga <mayorga.julian@gmail.com>
2013-10-08 16:24:46 -0300

    [Feature] Add CORS support

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,13 +1,15 @@
 'use strict';
 
 angular.module('leanTodoApp', ['ngResource'])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'TaskCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+.config(function ($routeProvider, $httpProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl: 'views/main.html',
+		controller: 'TaskCtrl'
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+	$httpProvider.defaults.useXDomain = true;
+	delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
